f534f25338113d88aa5bdc9daeac5a3ee2146423
Patrick Houle <phoule@bostontechnologies.com>
2017-04-09 15:53:31 -0400

    Initial Commit

---
+++ TestingNavigation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestingNavigation
+//
+//  Created by Patrick J. Houle on 4/9/17.
+//  Copyright © 2017 Patrick J. Houle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
